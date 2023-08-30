723af0a7c4a1b954a300a58f0af7b27a146ebd83
wuhuua <wuuhuua@163.com>
2023-08-30 21:29:00 +0800

    <feat>添加扩展threadLocal,线程安全的可线程池复用threadlocal

--- j2p/src/test/java/com/unloadhome/Basic/BasicTest.java
+++ j2p/src/test/java/com/unloadhome/Basic/BasicTest.java
@@ -8,7 +8,7 @@ public class BasicTest {
     @Test
     public void test_range_should_get_iterator_when_execute(){
         int num=0;
-        for(int i:new Range(5)){
+        for(Number i:new Range(5)){
             assertEquals(num,i);
             num++;
         }
